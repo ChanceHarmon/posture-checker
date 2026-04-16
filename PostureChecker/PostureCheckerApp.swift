9965c374072fdb0f853fcacfd00ad2ec9fa037fc
Chance Harmon <henrikillsit@gmail.com>
2026-04-16 14:33:17 -0600

    buttons and initial state setup

--- PostureChecker/PostureCheckerApp.swift
+++ PostureChecker/PostureCheckerApp.swift
@@ -10,9 +10,35 @@ import AppKit
 
 @main
 struct PostureCheckerApp: App {
+    @State private var isRunning: Bool = false
+    @State private var selectedInterval: Int = 15
     var body: some Scene {
         MenuBarExtra("PostureChecker", systemImage: "figure.walk") {
-            Text("Posture Reminders")
+            Text(isRunning ? "Status: Running" : "Status: Stopped")
+            Divider()
+            Button("Start"){
+                isRunning = true
+            }
+            Button("Stop"){
+                isRunning = false
+            }
+            Divider()
+            Button(action: {selectedInterval = 15}){
+                HStack {
+                    Text("15 mins")
+                    if selectedInterval == 15 {
+                        Image(systemName: "checkmark")
+                    }
+                }
+            }
+            Button(action: {selectedInterval = 20}){
+                HStack {
+                    Text("20 mins")
+                    if selectedInterval == 20 {
+                        Image(systemName: "checkmark")
+                    }
+                }
+            }
             Divider()
             Button("Quit"){
                 NSApplication.shared.terminate(nil)
